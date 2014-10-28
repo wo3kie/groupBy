a23661df05a1f13c3a741b2e7cb45d87c13767f2
Lukasz Czerwinski <wo3kie@hotmail.com>
2014-10-28 21:09:24 +0000

    Initial commit

---
+++ groupBy.h
@@ -0,0 +1,104 @@
+#ifndef _GROUP_BY_H_
+#define _GROUP_BY_H_
+
+#include <map>
+#include <vector>
+
+namespace details
+{
+    /*
+     * return_type
+     */
+
+    template< typename T >
+    struct return_type
+    {
+      typedef typename return_type< decltype( &T::operator() )>::type type;
+    };
+
+    template< typename R, typename C, typename... A >
+    struct return_type< R (C::*)(A...) > 
+    {
+        typedef R type;
+    };
+
+    template< typename R, typename C, typename... A >
+    struct return_type< R (C::*)(A...) const > 
+    {
+        typedef R type;
+    };
+
+    template< typename R, typename... A >
+    struct return_type< R (*)(A...) > 
+    {
+        typedef R type;
+    };
+}
+
+namespace details
+{
+    /*
+     * GroupBy
+     */
+
+    template< typename Arg, typename... Ts >
+    struct GroupBy;
+
+    template< typename Arg, typename T, typename... Ts >
+    struct GroupBy< Arg, T, Ts... >
+    {
+        typedef std::map<
+            typename details::return_type< T >::type,
+            typename GroupBy< Arg, Ts... >::return_type
+        > return_type;
+    };
+
+    template< typename Arg, typename T >
+    struct GroupBy< Arg, T >
+    {
+        typedef std::map<
+            typename return_type< T >::type,
+            std::vector< Arg >
+        > return_type;
+    };
+}
+
+namespace details
+{
+    /*
+     * groupByImpl
+     */
+
+    template< typename Map, typename Iterator, typename F >
+    void groupByImpl( Map & map, Iterator const current, F f )
+    {
+        map[ f( *current ) ].push_back( *current );
+    }
+
+    template< typename Map, typename Iterator, typename F, typename... Fs >
+    void groupByImpl( Map & map, Iterator const current, F f, Fs... fs )
+    {
+        groupByImpl( map[ f( *current ) ], current, fs... );
+    }
+}
+
+/*
+ * groupBy
+ */
+
+template< typename Iterator, typename F, typename... Fs >
+typename details::GroupBy< typename std::iterator_traits< Iterator >::value_type, F, Fs... >::return_type
+groupBy( Iterator begin, Iterator const end, const F& f, Fs... fs )
+{
+    typename details::GroupBy< typename std::iterator_traits< Iterator >::value_type, F, Fs... >::return_type result;
+
+    for( /* empty */ ; begin != end ; ++ begin )
+    {
+        details::groupByImpl( result, begin, f, fs... );
+    }
+
+    return result;
+}
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include "groupBy.h"
+
+#include <cassert>
+
+struct T
+{
+    int i;
+    float f;
+    char c;
+};
+
+bool operator==( T const & lhs, T const & rhs )
+{
+    return lhs.i == rhs.i && lhs.f == rhs.f && lhs.c == rhs.c;
+}
+
+int get_i( T const & t )
+{
+    return t.i;
+}
+
+struct Get_f
+{
+    float operator()( T const & t ) const
+    {
+        return t.f;
+    }
+};
+
+auto const get_c = []( T const & t )
+{
+    return t.c;
+};
+
+int main()
+{
+    std::vector<T> v{ {
+        T{ 1, 1.1f, 'a' },
+        T{ 2, 2.2f, 'b' },
+        T{ 2, 2.2f, 'c' },
+        T{ 3, 3.3f, 'd' },
+        T{ 3, 3.3f, 'e' },
+        T{ 3, 3.3f, 'f' } } };
+
+    {
+        std::map< int, std::vector< T > > const actual = groupBy( v.begin(), v.end(), & get_i );
+
+        std::map< int, std::vector< T > > const expected {
+          { { 1, { T{ 1, 1.1f, 'a' } } },
+            { 2, { T{ 2, 2.2f, 'b' }, T{ 2, 2.2f, 'c' } } },
+            { 3, { T{ 3, 3.3f, 'd' }, T{ 3, 3.3f, 'e' }, T{ 3, 3.3f, 'f' } } } }
+        };
+
+        assert( actual == expected );
+    }
+
+    {
+        std::map< int, std::map< float, std::vector< T > > > const actual
+            = groupBy( v.begin(), v.end(), & get_i, Get_f());
+
+        std::map< int, std::map< float, std::vector< T > > > const expected{
+            { { 1, { { 1.1f, { T{ 1, 1.1f, 'a' } } } } },
+              { 2, { { 2.2f, { T{ 2, 2.2f, 'b' }, T{ 2, 2.2f, 'c' } } } } },
+              { 3,
+                { { 3.3f,
+                    { T{ 3, 3.3f, 'd' }, T{ 3, 3.3f, 'e' },
+                      T{ 3, 3.3f, 'f' } } } } } }
+        };
+
+        assert( actual == expected );
+    }
+
+    {
+        std::map< int, std::map<float, std::map<char, std::vector<T> > > > const actual
+            = groupBy( v.begin(), v.end(), & get_i, Get_f(), get_c );
+
+        std::map< int, std::map< float, std::map< char, std::vector< T > > > > const expected {
+            { { 1, { { 1.1f, { { 'a', { T{ 1, 1.1f, 'a' } } } } } } },
+              { 2,
+                { { 2.2f,
+                    { { 'b', { T{ 2, 2.2f, 'b' } } },
+                      { 'c', { T{ 2, 2.2f, 'c' } } } } } } },
+              { 3,
+                { { 3.3f,
+                    { { 'd', { T{ 3, 3.3f, 'd' } } },
+                      { 'e', { T{ 3, 3.3f, 'e' } } },
+                      { 'f', { T{ 3, 3.3f, 'f' } } } } } } } } };
+
+        assert(actual == expected);
+    }
+}
+
